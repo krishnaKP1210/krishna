b52de35ea94d0531bdb8fd0e9f1233f26a155929
krishnaKP1210 <krishnapanchal983@gmail.com>
2021-11-29 22:43:28 +0530

    updated library

---
+++ library/__manifest__.py
@@ -0,0 +1,10 @@
+{
+	  'name' : 'Library' ,
+	  'category' : 'Sales' ,
+	  'application' : 'True' , 
+	  'data': [
+        'security/ir.model.access.csv',
+        'views/library_menus.xml',
+        'views/library_views.xml',      
+	],
+}
